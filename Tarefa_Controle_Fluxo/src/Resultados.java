ab3e8cbfa02e781ab8d0c073029e38661234d7c1
mauriciocampos1234 <156216039+mauriciocampos1234@users.noreply.github.com>
2024-10-18 15:05:50 -0300

    Mod 10

---
+++ Tarefa_Controle_Fluxo/src/Resultados.java
@@ -0,0 +1,38 @@
+/**
+ * @author MauricioCampos
+ */
+
+/**
+ * @author MauricioCampos
+ */
+
+public class Resultados {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 6;
+        int nota3 = 8;
+        int nota4 = 4;
+
+        int nota5 = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (nota5 >= 7) {
+            System.out.println("Aluno aprovado");
+        } else if (nota5 <= 5) {
+            System.out.println("Aluno reprovado");
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
